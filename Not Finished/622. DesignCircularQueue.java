6401560ea81bb1e21223486d632fd4509c89ae54
Musashiii <smarthgupta03@gmail.com>
2026-06-29 17:42:20 +0530

    Questions for Linux PC

---
+++ Not Finished/622. DesignCircularQueue.java
@@ -0,0 +1,69 @@
+import java.util.LinkedList;
+
+class MyCircularQueue {
+
+    int[] arr;
+    int front, rear;
+    int size, capacity;
+
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        capacity = k;
+        size = 0;
+        front = 0;
+        rear = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if(size < capacity){
+            arr[rear] = value;
+            rear = (rear + 1) % capacity;
+            size++;
+            return true;
+        } else
+            return false;
+    }
+
+    public boolean deQueue() {
+        if(size == 0)
+            return false;
+        else{
+            front = (front + 1) % capacity;
+            size--;
+            return true;
+        }
+    }
+
+    public int Front() {
+        if(size == 0)
+            return -1;
+        else
+            return arr[front];
+    }
+
+    public int Rear() {
+        if(size == 0)
+            return -1;
+        else
+            return arr[(rear - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
--- Not Finished/895. MaximumFrequencyStack.java
+++ Not Finished/895. MaximumFrequencyStack.java
@@ -1,14 +1,34 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 class FreqStack {
+
+    HashMap<Integer, Stack<Integer>> freqStack;
+    HashMap<Integer, Integer> frequency;
+    int maxFreq;
+
     public FreqStack() {
-        
+        freqStack = new HashMap<>();
+        frequency = new HashMap<>();
+        maxFreq = 0;
     }
-    
+
     public void push(int val) {
-        
+        frequency.put(val, frequency.getOrDefault(val, 0) + 1);
+        int freq = frequency.get(val);
+        if(!freqStack.containsKey(frequency.get(val)))
+            freqStack.put(freq, new Stack<>());
+        freqStack.get(freq).push(val);
+        maxFreq = Math.max(maxFreq, freq);
     }
-    
+
     public int pop() {
-        
+        int frequentElement = 0;
+        frequentElement = freqStack.get(maxFreq).pop();
+        frequency.put(frequentElement, frequency.get(frequentElement) - 1);
+        if(freqStack.get(maxFreq).isEmpty())
+            maxFreq--;
+        return frequentElement;
     }
 }
 
